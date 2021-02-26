aaf61ee97d37baf7e6475361b811a1b8a04a0170
James <jrmitsumasu@gmail.com>
2021-02-26 00:00:13 -0500

    Manifesting mantra phrases.

---
+++ mixedMessages/script.js
@@ -0,0 +1,36 @@
+
+console.log('I am amazing.');
+
+console.log('I am loved.');
+
+console.log('I am abundant.');
+
+console.log('I am rich and wealthy spirtually, financially, and in every other regard.');
+
+console.log('I am successful and destined to be successful. It is literally in my blood.');
+
+console.log('I attract and don\'t chase.');
+
+console.log('I am genuine.');
+
+console.log('I am courageous.');
+
+console.log('I am resilient.');
+
+console.log('I am strong.');
+
+console.log('I am wise.');
+
+console.log('I am smart.');
+
+console.log('I am intelligent.');
+
+console.log('I am healthy.');
+
+console.log('I am strong.');
+
+console.log('I am a reader.');
+
+console.log('I have perservence and grit.');
+
+console.log('I am a person of my word.');
